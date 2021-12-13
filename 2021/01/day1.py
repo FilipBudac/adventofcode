b8b4c74d884b7cbb930076535c436c552a601739
FilipBudac <filip.budac@gmail.com>
2021-12-13 20:03:24 +0100

    day 1

---
+++ 2021/01/day1.py
@@ -0,0 +1,14 @@
+def get_numbers():
+    with open('input.txt') as f:
+        numbers = [int(num) for num in f.readlines()]
+    return numbers
+
+
+def main():
+    numbers = get_numbers()
+    shifted_numbers = numbers[1:]
+    print(sum(1 if other > num else 0 for num, other in zip(numbers, shifted_numbers)))
+
+
+if __name__ == '__main__':
+    main()
