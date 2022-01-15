f05ba9dea9a0e2c321db41d24191dc0e576089ad
FilipBudac <filip.budac@gmail.com>
2022-01-15 20:10:33 +0100

    day6 2020 simplified

--- 2020/06/day6b.py
+++ 2020/06/day6b.py
@@ -1,13 +1,4 @@
 with open('input.txt') as file:
-    answer_groups = [group.splitlines() for group in file.read().split('\n\n')]
+    answers = [map(set, group.splitlines()) for group in file.read().split('\n\n')]
 
-question_groups = [set(''.join(group)) for group in answer_groups]
-
-answered_by_all = 0
-for answer_group, question_group in zip(answer_groups, question_groups):
-    respondents_count = len(answer_group)
-    for question in question_group:
-        if ''.join(answer_group).count(question) == respondents_count:
-            answered_by_all += 1
-
-print(answered_by_all)
+print(sum(len(set.intersection(*answer)) for answer in answers))
