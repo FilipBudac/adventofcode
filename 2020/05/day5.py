c0dd5fbf57140d7577b841ce472b092fc47d4135
FilipBudac <filip.budac@gmail.com>
2022-01-16 19:57:08 +0100

    day5 2020 refactoring

--- 2020/05/day5.py
+++ 2020/05/day5.py
@@ -1,20 +1,20 @@
 with open('input.txt') as file:
-    instructions = [list(ins.strip()) for ins in file]
+    instruction_groups = [list(instructions.strip()) for instructions in file]
 
 
-def calc_pos(instructions, dir, low, high):
+def calc_pos(instructions, direction, low, high):
     ins = instructions.pop(0)
-    if ins == dir:
-        diff = abs(low - high)
+    if ins == direction:
+        diff = high - low
         low, high = (low, low + diff // 2)
     else:
-        diff = abs(low - high)
+        diff = high - low
         low, high = (low + round(diff / 2), high)
 
     if not instructions:
-        return low if ins == dir else high
+        return low if ins == direction else high
 
-    return calc_pos(instructions, dir, low, high)
+    return calc_pos(instructions, direction, low, high)
 
 
-print(max(calc_pos(ins[0:7], 'F', 0, 127) * 8 + calc_pos(ins[7:], 'L', 0, 7) for ins in instructions))
+print(max(calc_pos(instructions[0:7], 'F', 0, 127) * 8 + calc_pos(instructions[7:], 'L', 0, 7) for instructions in instruction_groups))
--- 2020/05/day5b.py
+++ 2020/05/day5b.py
@@ -1,33 +1,36 @@
 with open('input.txt') as file:
-    instructions = [list(ins.strip()) for ins in file]
+    instruction_groups = [list(instructions.strip()) for instructions in file]
 
 
-def calc_pos(instructions, dir, low, high):
+def calc_pos(instructions, direction, low, high):
     ins = instructions.pop(0)
-    if ins == dir:
-        diff = abs(low - high)
+    if ins == direction:
+        diff = high - low
         low, high = (low, low + diff // 2)
     else:
-        diff = abs(low - high)
+        diff = high - low
         low, high = (low + round(diff / 2), high)
 
     if not instructions:
-        return low if ins == dir else high
+        return low if ins == direction else high
 
-    return calc_pos(instructions, dir, low, high)
+    return calc_pos(instructions, direction, low, high)
 
 
-ids = [calc_pos(ins[0:7], 'F', 0, 127) * 8 + calc_pos(ins[7:], 'L', 0, 7) for ins in instructions]
-max_id = max(ids)
-min_id = min(ids)
+def my_id(ids):
+    """
+    my_id = total_sum - actual_sum, where
+      total_sum -> n(n+1)/2 (since we are not starting from 1, we need to adjust an interval of ids n(n+1)/2 - n(n-1)/2)
+      actual_sum -> sum(ids)
+    """
+    max_id = max(ids)
+    min_id = min(ids)
 
-# total_sum - actual_sum
-# total_sum -> n(n+1)/2
-# actual_sum -> sum(ids)
-# we are not starting from 1, so we need to adjust an interval of ids
-# n(n+1)/2 - n(n-1)/2
-total_sum = (max_id * (max_id + 1) // 2) - (min_id * (min_id - 1) // 2)
-actual_sum = sum(ids)
-my_id = total_sum - actual_sum
+    total_sum = (max_id * (max_id + 1) // 2) - (min_id * (min_id - 1) // 2)
+    actual_sum = sum(ids)
 
-print(my_id)
+    return total_sum - actual_sum
+
+
+print(my_id([calc_pos(instructions[0:7], 'F', 0, 127) * 8 + calc_pos(instructions[7:], 'L', 0, 7) for instructions in instruction_groups]))
+
