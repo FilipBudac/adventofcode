b092e0c5227a0a51779291e5d0de51b43f439f8d
Filip Budac <filip.budac@gmail.com>
2022-05-22 10:36:13 +0200

    day15 2020

---
+++ 2020/15/day15b.py
@@ -0,0 +1,14 @@
+LAST_TURN = 30000000
+
+
+with open('input.txt') as file:
+    nums = list(map(int, file.readline().split(',')))
+
+spoken = {num: i + 1 for i, num in enumerate(nums)}
+last = nums[-1]
+for turn in range(len(nums), LAST_TURN):
+    next_turn = turn - spoken[last] if last in spoken else 0
+    spoken[last] = turn
+    last = next_turn
+
+print(last)
