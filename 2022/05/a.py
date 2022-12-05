2b199c094ef60a23dee1bc2a1c794e0016fedaad
FilipBudac <filip.budac@gmail.com>
2022-12-05 08:19:37 +0100

    day05 2022

---
+++ 2022/05/a.py
@@ -0,0 +1,19 @@
+import collections
+import itertools
+import re
+
+
+with open('input.txt') as file:
+    cargos, ins = file.read().split('\n\n')
+
+state = collections.defaultdict(list)
+for line in itertools.zip_longest(*cargos.splitlines()):
+    num, *letters = reversed(line)
+    if num and num.isnumeric():
+        state[int(num)].extend([l for l in letters if l not in [' ', None]])
+
+for (size, f, t) in [[*map(int, re.findall(r'\d+', i))] for i in ins.splitlines()]:
+    state[t].extend(reversed(state[f][-size:]))
+    del state[f][-size:]
+
+print(''.join(v[-1] for v in state.values()))
