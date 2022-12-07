e8fabb0b9bfadf33571e2d495484ae3d82e65895
FilipBudac <filip.budac@gmail.com>
2022-12-07 08:48:40 +0100

    day07 2022

---
+++ 2022/07/a.py
@@ -0,0 +1,21 @@
+import collections
+import itertools
+
+with open('input.txt') as file:
+    lines = map(str.rstrip, file.read().splitlines())
+
+dirs_size = collections.defaultdict(int)
+dirs = []
+for l in lines:
+    if l.endswith('ls') or l.startswith('dir'):
+        continue
+    a, *parts = l.split()
+    if a.isnumeric():
+        file, = parts
+        for p in itertools.accumulate(dirs, func=lambda d, n: d + '/' + n):
+            dirs_size[p] += int(a)
+    else:
+        _, dest = parts
+        dirs.pop() if dest == '..' else dirs.append(dest)
+
+print(sum(t for t in dirs_size.values() if t <= 100000))
