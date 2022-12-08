d84ecf986c41ef5ca253ed174c7fce9c8a5053ca
FilipBudac <filip.budac@gmail.com>
2022-12-08 15:44:58 +0100

    day08 2022 refactoring

--- 2022/08/a.py
+++ 2022/08/a.py
@@ -1,22 +1,16 @@
-
 with open('input.txt') as file:
     grid = {(i, j): int(num) for i, line in enumerate(file) for j, num in enumerate(line.rstrip())}
 
-
-m = max(x for (x, y) in grid)
-
 tot = 0
+size = max(x for (x, y) in grid)
 for (x, y), num in grid.items():
-    if x in [0, m] or y in [0, m]:
-        tot += 1
-        continue
-    if all(grid[x + i + 1, y] < num for i in range(m - x)):
-        tot += 1
-    elif all(grid[x - i, y] < num for i in range(x, 0, -1)):
-        tot += 1
-    elif all(grid[x, y + i + 1] < num for i in range(m - y)):
-        tot += 1
-    elif all(grid[x, y - i] < num for i in range(y, 0, -1)):
+    if (
+        all(grid[x + i + 1, y] < num for i in range(size - x)) or
+        all(grid[x, y + i + 1] < num for i in range(size - y)) or
+        all(grid[x - i - 1, y] < num for i in range(x)) or
+        all(grid[x, y - i - 1] < num for i in range(y)) or
+        x in (0, size) or y in (0, size)
+    ):
         tot += 1
 
 print(tot)
--- 2022/08/b.py
+++ 2022/08/b.py
@@ -3,36 +3,26 @@ import functools
 with open('input.txt') as file:
     grid = {(i, j): int(num) for i, line in enumerate(file) for j, num in enumerate(line.rstrip())}
 
-m = max(x for (x, y) in grid)
-
 tot = []
+size = max(x for (x, y) in grid)
 for (x, y), num in grid.items():
-    if x in [0, m] or y in [0, m]:
-        continue
     c = 4 * [0]
-    for i in range(m - x):
-        if grid[x + i + 1, y] >= num:
-            c[0] += 1
-            break
+    for i in range(size - x):
         c[0] += 1
-
+        if grid[x + i + 1, y] >= num:
+            break
     for i in range(x):
-        if grid[x - i - 1, y] >= num:
-            c[1] += 1
-            break
         c[1] += 1
-
-    for i in range(m - y):
-        if grid[x, y + i + 1] >= num:
-            c[2] += 1
+        if grid[x - i - 1, y] >= num:
             break
+    for i in range(size - y):
         c[2] += 1
-
-    for i in range(y):
-        if grid[x, y - i - 1] >= num:
-            c[3] += 1
+        if grid[x, y + i + 1] >= num:
             break
+    for i in range(y):
         c[3] += 1
-    tot.append(functools.reduce((lambda x, y: x * y), c))
+        if grid[x, y - i - 1] >= num:
+            break
+    tot.append(functools.reduce((lambda a, b: a * b), c))
 
 print(max(tot))
