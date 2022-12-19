e8f7b3d8ffdfcc85b982eecffec009fa972c3999
FilipBudac <filip.budac@gmail.com>
2022-12-19 20:39:24 +0100

    day19 2022

---
+++ 2022/19/a.py
@@ -0,0 +1,39 @@
+import re
+
+
+def build(oc, cc, ooc, occ, goc, gobc, t):
+    s = [(0, 1, 0, 0, 0, 0, 0, 0, t)]
+    seen = set()
+    m = 0
+    req = max(oc, cc, ooc, goc)
+    while s:
+        cur = s.pop()
+        if cur in seen:
+            continue
+        seen.add(cur)
+
+        o, ro, c, rc, ob, rob, g, rg, t = cur
+        ro = min(ro, req)
+        no, nc, nob, ng = o + ro, c + rc, ob + rob, g + rg
+        t -= 1
+        if t == 0:
+            m = max(m, ng)
+            continue
+
+        if o >= goc and ob >= gobc:
+            s.append((no - goc, ro, nc, rc, nob - gobc, rob, ng, rg + 1, t))
+        if o >= ooc and c >= occ:
+            s.append((no - ooc, ro, nc - occ, rc, nob, rob + 1, ng, rg, t))
+        if o >= cc:
+            s.append((no - cc, ro, nc, rc + 1, nob, rob, ng, rg, t))
+        if o >= oc:
+            s.append((no - oc, ro + 1, nc, rc, nob, rob, ng, rg, t))
+        s.append((no, ro, nc, rc, nob, rob, ng, rg, t))
+
+    return m
+
+
+with open('input.txt') as file:
+    bp = [[*map(int, re.findall(r'\d+', l))] for l in file]
+
+print(sum(_id * build(*nums, 24) for _id, *nums in bp))
---
+++ 2022/19/b.py
@@ -0,0 +1,48 @@
+import operator
+import re
+import functools
+
+
+def build(oc, cc, ooc, occ, goc, gobc, t):
+    s = [(0, 1, 0, 0, 0, 0, 0, 0, t)]
+    seen = set()
+    m = 0
+    req = max(oc, cc, ooc, goc)
+    while s:
+        cur = s.pop()
+        if cur in seen:
+            continue
+        seen.add(cur)
+
+        o, ro, c, rc, ob, rob, g, rg, t = cur
+        ro = min(ro, req)
+        no, nc, nob, ng = o + ro, c + rc, ob + rob, g + rg
+        t -= 1
+        if t == 0:
+            m = max(m, ng)
+            continue
+
+        ext = False
+        if o >= goc and ob >= gobc:
+            s.append((no - goc, ro, nc, rc, nob - gobc, rob, ng, rg + 1, t))
+            ext = True
+        if o >= ooc and c >= occ:
+            s.append((no - ooc, ro, nc - occ, rc, nob, rob + 1, ng, rg, t))
+            ext = True
+        if o >= cc:
+            s.append((no - cc, ro, nc, rc + 1, nob, rob, ng, rg, t))
+            ext = True
+        if o >= oc:
+            s.append((no - oc, ro + 1, nc, rc, nob, rob, ng, rg, t))
+            ext = True
+
+        if not ext:
+            s.append((no, ro, nc, rc, nob, rob, ng, rg, t))
+
+    return m
+
+
+with open('input.txt') as file:
+    bp = [[*map(int, re.findall(r'\d+', l))] for l in file]
+
+print(functools.reduce(operator.mul, [build(*nums, 32) for _, *nums in bp[:3]]))
